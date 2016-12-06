7b54c272690b62d15fe66eb7fe9b6006ef00f4d1
Selina Agbayani <sva@mail.ubc.ca>
2016-12-05 16:18:43 -0800

    Commit from FISH - hw09 - fixinf custom function

--- R/random_sample_table.R
+++ R/random_sample_table.R
@@ -7,23 +7,26 @@
 #'
 #' @return data.frame
 #' @export
-#' @import dplyr
+#' @import dplyr, assertthat
+#'  
 #' @examples
 #' # randomly sample rows from a table (by default 10)
 #' peek(iris)
 #' # randomly sample 10 rows from a table
 #' peek(iris, 10)
-random_sample_table <- function(x, nsample=15){
-  
-  # Verify nsample is integer
-  stopifnot(is.integer(nsample))
-  
-  sample_table  <- x %>% 
-    dplyr::as.tbl() %>%
-    dplyr::mutate(original_nrow = 1:nrow(.)) %>%
-    dplyr::sample_n(nsample) %>%
-    as.data.frame()
-  
+
+
+
+random_sample_table <- function(x, nsample=5){
+
+  # Verify nsample is numeric
+  assertthat::assert_that(assertthat::is.count(nsample))
+
+  sample_table  <- x %>%
+      dplyr::data_frame(x)
+      dplyr::mutate(original_nrow = 1:nrow(.)) %>%
+      dplyr::sample_n(nsample)
+
   return(sample_table)
 }
 
